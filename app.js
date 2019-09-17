5d37e881e15392baaf6ac2de708bb9178e045503
CN0104 <cn0104@CN11.local>
2019-09-17 09:53:57 +0100

    change media quer

---
+++ app.js
@@ -0,0 +1,16 @@
+// Jquery scroll
+$('.nav-links a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body')
+      .animate({
+        scrollTop: $(hash).offset().top
+      },
+      1000
+      );
+  }
+});
+
